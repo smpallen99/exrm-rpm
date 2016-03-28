d3b3b81537d4745785480f5072b53df10b87fcfc
Stepan Tezyunichev <stepan.tezyunichev@gmail.com>
2016-03-28 20:24:56 +0300

    fixing: Use Project.config instead of Project.project

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExrmRpm.Mixfile do
 
   def project do
     [app: :exrm_rpm,
-     version: "0.3.0",
+     version: "0.3.2",
      elixir: "~> 1.0",
      description: description,
      package: package,
