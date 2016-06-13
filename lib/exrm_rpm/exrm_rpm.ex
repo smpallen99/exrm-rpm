c9cc24f60ef4b2b486fe8f38b848eaf83ab9113c
Valeri Sokolov <valeri.sokolov@derivco.se>
2016-06-13 13:13:44 +0200

    made monotonously increasing pre-release and release versions

--- lib/exrm_rpm/exrm_rpm.ex
+++ lib/exrm_rpm/exrm_rpm.ex
@@ -115,8 +115,23 @@ defmodule ReleaseManager.Plugin.Rpm do
     config
   end
 
-  defp normalize_version(version) do
-    version |> String.split([".", "-"]) |> Enum.map(fn(segment) ->  Regex.replace(~r/[^0-9]+/, segment, "") end) |> Enum.join(".")
+  defp normalize_version(version) when is_binary(version) do
+    version |> String.split([".", "-"]) |> normalize_version |> Enum.join(".")
+  end
+  defp normalize_version(v = [_maj, _min, _patch]) do
+    v
+  end
+  defp normalize_version(v = [maj, _min, _patch, _pre]) when is_binary(maj) do
+    normalize_version(v |> Enum.map(fn(segment) -> Regex.replace(~r/[^0-9]+/, segment, "") |> String.to_integer end))
+  end
+  defp normalize_version([maj, 0, 0, pre]) when maj > 0 do
+    [maj - 1, 99, 99, 99, pre]
+  end
+  defp normalize_version([maj, min, 0, pre]) when min > 0 do
+    [maj, min - 1, 99, 99, pre]
+  end
+  defp normalize_version([maj, min, patch, pre]) when patch > 0 do
+    [maj, min, patch - 1, 99, pre]
   end
 
   defp copy_extra_sources(config) do
